4392d3b31c869d50acf4f5710aa8696b9e5c5024
tutuut <814146171@qq.com>
2020-08-06 21:39:40 +0800

    new shipping address

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserAddressRequest;
 use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
@@ -19,8 +20,31 @@ class UserAddressesController extends Controller
         ]);
     }
 
-    public function create(UserAddress $userAddress)
+    /**
+     * @param UserAddress $address
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create(UserAddress $address)
     {
-        return view('user_addresses.create_and_edit', compact('userAddress'));
+        return view('user_addresses.create_and_edit', compact('address'));
+    }
+
+    /**
+     * 新增地址
+     * @param UserAddressRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+        return redirect()->route('user_addresses.index');
     }
 }
