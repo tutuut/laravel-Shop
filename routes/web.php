42efa5495f0f8b256fdd049506cc1114004c5019
tutuut <814146171@qq.com>
2020-08-06 13:43:11 +0800

    email verify

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
