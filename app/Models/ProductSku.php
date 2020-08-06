ff40fbd728b0408c515f10206b097c8e08455f7f
tutuut <814146171@qq.com>
2020-08-06 23:06:41 +0800

    product model

---
+++ app/Models/ProductSku.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
