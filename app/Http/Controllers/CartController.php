129c20179333a8b6d6eb872d76e463199b08611a
tutuut <814146171@qq.com>
2020-08-08 21:31:16 +0800

    package business code

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,10 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\ProductSku;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
-use App\Models\CartItem;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    //利用 Laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $cartItems = $this->cartService->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
+
+        return view('cart.index', compact('cartItems', 'addresses'));
+    }
+
     /**
      * 添加购物车
      * @param AddCartRequest $request
@@ -16,45 +36,15 @@ class CartController extends Controller
      */
     public function add(AddCartRequest $request)
     {
-        $user = $request->user();
-        $skuId = $request->input('sku_id');
-        $amount = $request->input('amount');
-
-        //从数据库中查询该商品是否已经在购物车中
-        if($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-            //如果存在则直接叠加商品数量
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-            //否则创建一个新的购物车记录
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
 
         return [];
-    }
-
-    /**
-     * 购物车列表
-     * @param Request $request
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index(Request $request)
-    {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
-        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
-
-        return view('cart.index', compact('cartItems', 'addresses'));
     }
 
     public function remove(Request $request, ProductSku $sku)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
 
         return [];
     }
-
 }
