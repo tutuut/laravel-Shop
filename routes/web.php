ada3e220fd3f82bec42602471dfcb55079a21867
tutuut <814146171@qq.com>
2020-08-09 10:30:41 +0800

    order paid

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,17 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     //订单详情
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
+    //支付订单
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')
+        ->name('payment.alipay');
+    //支付返回结果
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')
+        ->name('payment.alipay.return');
 });
 
 //和products.favorites冲突
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
+//服务器端回调的路由不能放到带有 auth 中间件的路由组中，因为支付宝的服务器请求不会带有认证信息。
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')
+    ->name('payment.alipay.notify');
